ebe6d3126a611e3e86a8497d1ded6377ef301bfa
Parasmani Jain <parasmani.j@HCL.COM>
2018-12-18 21:37:47 +0530

    adding accordion group component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AccordionGroupComponent } from './components/accordion/accordion-group.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccordionGroupComponent,
+    AccordionComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/accordion/accordion-group.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'group',
+  templateUrl: 'accordion.component.html',
+  styleUrls: ['accordion.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AccordionGroupComponent {
+
+  /**
+   * If the panel is opened or closed
+   */
+  @Input() opened = false;
+
+  /**
+   * Text to display in the group title bar
+   */
+  @Input() title: string;
+
+  @Input() icons: string;
+
+  /**
+   * Emitted when user clicks on group titlebar
+   * @type {EventEmitter<any>}
+   */
+  @Output() toggle: EventEmitter<any> = new EventEmitter<any>();
+}
